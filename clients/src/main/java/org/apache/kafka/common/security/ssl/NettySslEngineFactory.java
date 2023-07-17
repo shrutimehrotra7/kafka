3e69f5f22ac04ad9bbb2ecd8e797308dbda363d0
Divij Vaidya <diviv@amazon.com>
2023-07-17 18:39:43 +0200

    Fix a bug with stmt checking for openssl native

--- clients/src/main/java/org/apache/kafka/common/security/ssl/NettySslEngineFactory.java
+++ clients/src/main/java/org/apache/kafka/common/security/ssl/NettySslEngineFactory.java
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.kafka.common.security.ssl;
 
 import io.netty.buffer.UnpooledByteBufAllocator;
@@ -6,7 +22,6 @@ import io.netty.handler.ssl.SslContext;
 import io.netty.handler.ssl.SslContextBuilder;
 import io.netty.handler.ssl.SslProtocols;
 import io.netty.handler.ssl.SslProvider;
-import io.netty.internal.tcnative.SSL;
 import org.apache.kafka.common.KafkaException;
 import org.apache.kafka.common.config.SslConfigs;
 import org.apache.kafka.common.config.types.Password;
@@ -69,13 +84,13 @@ public class NettySslEngineFactory implements SslEngineFactory {
 
     @Override
     public void configure(Map<String, ?> configs) {
-        if (OpenSsl.isAvailable()) {
+        if (!OpenSsl.isAvailable()) {
             throw new RuntimeException("OpenSSL is not supported");
         }
-        if (SslProvider.isTlsv13Supported(OPENSSL)) {
+        if (!SslProvider.isTlsv13Supported(OPENSSL)) {
             throw new RuntimeException("Tlsv13 not supported");
         }
-        if (SslProvider.isAlpnSupported(SslProvider.OPENSSL)) {
+        if (!SslProvider.isAlpnSupported(SslProvider.OPENSSL)) {
             throw new RuntimeException("Alpn is not supported");
         }
 
